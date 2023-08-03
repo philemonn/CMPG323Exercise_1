d034541610fb7db63877921fd6eccdd4d064e034
philemonn <105682301+philemonn@users.noreply.github.com>
2023-08-03 08:57:34 +0200

    Add project files.

---
+++ PassFailDestinction/Form1.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace PassFailDestinction
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonCalculate = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.comboBoxgrade = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // buttonCalculate
+            // 
+            this.buttonCalculate.Location = new System.Drawing.Point(127, 67);
+            this.buttonCalculate.Name = "buttonCalculate";
+            this.buttonCalculate.Size = new System.Drawing.Size(75, 23);
+            this.buttonCalculate.TabIndex = 0;
+            this.buttonCalculate.Text = "Calculate";
+            this.buttonCalculate.UseVisualStyleBackColor = true;
+            this.buttonCalculate.Click += new System.EventHandler(this.buttonCalculate_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(32, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(148, 16);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "What is your grade(/10):";
+            // 
+            // comboBoxgrade
+            // 
+            this.comboBoxgrade.FormattingEnabled = true;
+            this.comboBoxgrade.Items.AddRange(new object[] {
+            "0",
+            "1",
+            "2",
+            "3",
+            "4",
+            "5",
+            "6",
+            "7",
+            "8",
+            "9",
+            "10"});
+            this.comboBoxgrade.Location = new System.Drawing.Point(186, 23);
+            this.comboBoxgrade.Name = "comboBoxgrade";
+            this.comboBoxgrade.Size = new System.Drawing.Size(53, 24);
+            this.comboBoxgrade.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(320, 135);
+            this.Controls.Add(this.comboBoxgrade);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.buttonCalculate);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonCalculate;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox comboBoxgrade;
+    }
+}
+
---
+++ PassFailDestinction/Form1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PassFailDestinction
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonCalculate_Click(object sender, EventArgs e)
+        {
+            if(comboBoxgrade.SelectedIndex==0)
+            {
+                MessageBox.Show("You have failed");
+            }
+
+            if (comboBoxgrade.SelectedIndex == 1)
+            {
+                MessageBox.Show("You have failed");
+            }
+            if (comboBoxgrade.SelectedIndex == 2)
+            {
+                MessageBox.Show("You have failed");
+            }
+            if (comboBoxgrade.SelectedIndex == 3)
+            {
+                MessageBox.Show("You have failed");
+            }
+            if (comboBoxgrade.SelectedIndex == 4)
+            {
+                MessageBox.Show("You have failed");
+            }
+            if (comboBoxgrade.SelectedIndex == 5)
+            {
+                MessageBox.Show("You have passed");
+            }
+            if (comboBoxgrade.SelectedIndex == 6)
+            {
+                MessageBox.Show("You have passed");
+            }
+            if (comboBoxgrade.SelectedIndex == 7)
+            {
+                MessageBox.Show("You have passed");
+            }
+            if (comboBoxgrade.SelectedIndex == 8)
+            {
+                MessageBox.Show("You have passed, with distinction");
+            }
+            if (comboBoxgrade.SelectedIndex == 9)
+            {
+                MessageBox.Show("You have passed, with distinction");
+            }
+            if (comboBoxgrade.SelectedIndex == 10)
+            {
+                MessageBox.Show("You have passed, with distinction");
+            }
+        }
+    }
+}
